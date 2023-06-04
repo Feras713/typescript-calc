9cc8163507ddcab0788da096e11a78a6e9c04fc1
Feras713 <feras.713@gmail.com>
2023-06-04 21:13:40 +0300

    first commit

---
+++ build/main.js
@@ -0,0 +1,18 @@
+// Variables
+var a;
+a = 200;
+var b;
+b = 200;
+b = 'feras';
+b = new Date();
+var c;
+c = true;
+c = "hello";
+c = 145;
+var f;
+f = "hello";
+f = 250;
+function func(num, name) {
+    console.log(name + " is " + num + " years old");
+}
+func(30, 'feras');
---
+++ src/main.ts
@@ -0,0 +1,23 @@
+// Variables
+let a: number;
+a = 200;
+
+let b: number | string | Date;
+b = 200;
+b = 'feras';
+b = new Date();
+
+let c: any;
+c = true;
+c = "hello";
+c = 145;
+
+let f;
+f = "hello";
+f = 250;
+
+function func(num: number, name: string) {
+    console.log(name + " is " + num + " years old");
+}
+
+func(30, 'feras');
